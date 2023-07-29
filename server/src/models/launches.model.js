eae4d41196312b4633fc3e7ff5475a4fdd4e62f2
Priyanshu Naudiyal <87259279+sickbrat@users.noreply.github.com>
2023-07-29 12:52:51 +0530

    first commit

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,51 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: "Kepler Explortion X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("March 5, 2025"),
+  target: "Kepler-442 b",
+  customer: ["Priyanshu", "Apple"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["Priyanshu", "Apple"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
+module.exports = {
+  existsLaunchWithId,
+  getAllLaunches,
+  addNewLaunch,
+  abortLaunchById,
+};
